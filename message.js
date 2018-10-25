05e3f67dc7afd79bcad47c598d64fc9c412a6fb6
Toby Marsden <toby.m@rsden.com>
2018-10-25 17:53:43 +0200

    More message examples

--- message.js
+++ message.js
@@ -1,1 +1,5 @@
 ipc.of[socketId].emit('message', { action: 'refresh', image: '/home/pi/Code/wollemi-node/multicolor.png' });
+
+ipc.of[socketId].emit('message', { action: 'refresh', image: '/Users/toby/Code/wollemi-node/640x384_landscape_1bpp.png' });
+
+ipc.of[socketId].emit('message', { action: 'refresh', image: './640x384_landscape_1bpp.png' });
